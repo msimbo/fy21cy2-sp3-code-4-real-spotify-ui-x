bae1ba40ee5c89f6580cd1fc28347dc46bb906ee
Jasminethecreator <Jasminebienaime@outlook.com>
2021-06-25 16:43:07 -0400

    change image folder directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+
+    extend: {
+      fontFamily: {
+        body:['Questrial']
+      }
+    },
   },
   variants: {
     extend: {},
